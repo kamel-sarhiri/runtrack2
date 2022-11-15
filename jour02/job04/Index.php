c82e313f93c44abbcb94d8b44e3ca7603b6785dc
kamel-sarhiri <kamel.sarhiri@laplateforme.io>
2022-11-15 12:09:34 +0100

    correction ordre des conditions

--- jour02/job04/Index.php
+++ jour02/job04/Index.php
@@ -10,23 +10,23 @@
     <h1>Jour2, Job04</h1>
     <p><?php for ($i = 0; $i<=100; $i++) { //boucle for qui affiche les nombres de 1 à 100
 
-                if ($i===$i%3) { //affichage des nombres de 1 à 100 divisibles par 3
+                if ($i%3==0 AND $i%5==0) { //affichage des multiples de 3 et 5
+                    echo "FizzBuzz";
+                    echo "<br>"; //saut de ligne
+                    }
+                else if ($i%3==0) { //affichage des multiples de 3
                     echo "Fizz";
                     echo "<br>";
                 }
-                else if ($i===$i%5) { //affichage des nombres de 1 à 100 divisibles par 5
-                    echo "Buzz"; 
+                else if ($i%5==0) { //affichage des multiples de 5
+                    echo "Buzz";
                     echo "<br>"; //saut de ligne
-                }
-                else if ($i===$i%3 AND $i===$i%5) { //affichage des autres nombres
-                    echo "FizzBuzz"; 
-                    echo "<br>"; //saut de ligne
-                    }
                 }
                 else { //affichage des autres nombres
                     echo $i; //affichage des autres nombres
                     echo "<br>"; //saut de ligne
                     }
+                }
 
         ?>
     </p>
