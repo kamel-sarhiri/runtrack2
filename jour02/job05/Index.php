6191a66434e3106de0e95e5b5939902981895afc
kamel-sarhiri <kamel.sarhiri@laplateforme.io>
2022-11-15 15:17:51 +0100

    ajout de commentaires

--- jour02/job05/Index.php
+++ jour02/job05/Index.php
@@ -13,11 +13,11 @@
         for ($i=2; $i<=1000; $i++) {
             $nbDiv = 0;//Et on compte le nombre de diviseurs
             for ($j=1; $j<=$i; $j++) {
-                if ($i%$j==0) {
+                if ($i%$j==0) {   //Si le reste de la division est égal à 0, alors le nombre est divisible
                     $nbDiv++;
                 }
             }
-            if($nbDiv == 2){
+            if($nbDiv == 2){ //si le nombre de diviseur est égal à 2, alors le nombre est premier
     //Un nombre premier est un chiffre qui ne possède que 2 diviseur (1 et
     // lui-même)
                 echo $i." est premier<br>";
