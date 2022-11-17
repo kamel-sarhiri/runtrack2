af89839573cd854421c63a62db28f73479e7d86d
kamel-sarhiri <kamel.sarhiri@laplateforme.io>
2022-11-17 15:14:29 +0100

    réalisation job06

---
+++ jour04/job05/Index.php
@@ -0,0 +1,40 @@
+<!DOCTYPE html>
+<html lang="fr">
+
+<head>
+    <meta charset="UTF-8">
+    <meta http-equiv="X-UA-Compatible" content="IE=edge">
+    <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <title>Runtrack2</title>
+</head>
+<body>
+    <h1>Jour4, Job05</h1>
+    <p><u>Formulaire type Post</u></p>
+    <p>Nous n'utilisons pas GET pour ne pas afficher le mot de passe dans la barre de recherche</p>
+    
+
+    <form  action="" method="post">
+        <label for="username">Nom d'utilisateur :</label>
+        <input type="text" name="username" id="username">
+        <br>
+        <br>
+        <label for="password">Mot de passe :</label>
+        <input type="text" name="password" id="password">
+        <br>
+        <br>
+        <input type="submit" value="Envoyer">
+
+    </form>
+
+    <?php
+        if (isset($_POST['username']) && isset($_POST['password'])) {
+            if ($_POST['username'] == "John" && $_POST['password'] == "Rambo") {
+                echo "C'est pas ma guerre";
+            } else {
+                echo "Votre pire cauchemar";
+            }
+        }
+    ?>
+
+</body>
+</html>
---
+++ jour04/job06/Index.php
@@ -0,0 +1,38 @@
+<!DOCTYPE html>
+<html lang="fr">
+
+<head>
+    <meta charset="UTF-8">
+    <meta http-equiv="X-UA-Compatible" content="IE=edge">
+    <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <title>Runtrack2</title>
+</head>
+<body>
+    <h1>Jour4, Job06</h1>
+    <p><u>Formulaire type Get</u></p>
+    
+
+    <form  action="" method="get">
+        <label for="nombre">Nombre :</label>
+        <input type="text" name="nombre" id="nombre">
+        <br>
+        <br>
+        <input type="submit" value="Envoyer">
+
+    </form>
+
+    <?php
+        if (isset($_GET['nombre'])) {
+            $nombre = $_GET['nombre'];
+            if ($nombre%2 == 0) {
+                echo "Le nombre est pair";
+            } else {
+                echo "Le nombre est impair";
+            }
+           
+        }
+    
+    ?>
+
+</body>
+</html>
