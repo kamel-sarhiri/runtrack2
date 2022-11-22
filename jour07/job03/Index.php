0b47bcef18a11725fb883fb65516252059057ad9
kamel-sarhiri <kamel.sarhiri@laplateforme.io>
2022-11-22 11:26:07 +0100

    fonction getHello réalisée

---
+++ jour07/job03/Index.php
@@ -0,0 +1,28 @@
+<?php
+    function getHello() {
+        return "Hello LaPlateforme!";
+    }
+?>
+
+<!DOCTYPE html>
+<html lang="fr">
+<head>
+    <meta charset="UTF-8">
+    <meta http-equiv="X-UA-Compatible" content="IE=edge">
+    <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <title>jour 7 job03</title>
+</head>
+<body>
+    <h1>jour 7 job03</h1>
+    <br>
+    <h3>Les fonctions :</h3>
+    <br>
+
+        <?php
+        $str=getHello();
+        echo $str;
+        ?>
+
+</body>
+</html>
+
