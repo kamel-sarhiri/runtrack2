523dbd5da8f9471ea0b18d269fe2bea55336c316
kamel-sarhiri <kamel.sarhiri@laplateforme.io>
2022-11-22 10:38:54 +0100

    fonction hello

---
+++ jour07/job01/Index.php
@@ -0,0 +1,28 @@
+
+<?php
+    hello () {
+        echo "Hello LaPlateforme!";
+    }
+?>
+
+<!DOCTYPE html>
+<html lang="fr">
+<head>
+    <meta charset="UTF-8">
+    <meta http-equiv="X-UA-Compatible" content="IE=edge">
+    <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <title>jour 7 job01</title>
+</head>
+<body>
+    <h1>jour 7 job01</h1>
+    <br>
+    <h3>Input type texte :</h3>
+    <br>
+
+        <?php
+        hello();
+        ?>
+
+</body>
+</html>
+
