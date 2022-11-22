4b1cc3f44745b59ddbd17ec2ef5393ea7db392e2
kamel-sarhiri <kamel.sarhiri@laplateforme.io>
2022-11-22 15:56:17 +0100

    réalisation job05

--- jour07/job04/Index.php
+++ jour07/job04/Index.php
@@ -43,7 +43,7 @@
     <meta charset="UTF-8">
     <meta http-equiv="X-UA-Compatible" content="IE=edge">
     <meta name="viewport" content="width=device-width, initial-scale=1.0">
-    <title>jour 7 job03</title>
+    <title>jour 7 job04</title>
 </head>
 <body>
     <h1>jour 7 job04</h1>
---
+++ jour07/job05/Index.php
@@ -0,0 +1,51 @@
+
+<!DOCTYPE html>
+<html lang="fr">
+<head>
+    <meta charset="UTF-8">
+    <meta http-equiv="X-UA-Compatible" content="IE=edge">
+    <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <title>jour 7 job05</title>
+</head>
+<body>
+    <h1>jour 7 job05</h1>
+    <br>
+    <h3>Les fonctions :</h3>
+    <br>
+    <form  action="" method="get">
+        <label for="mot">Entrez un mot :</label>
+        <input type="text" name="mot" id="mot">
+        <br>
+        <br>
+        <label for="lettre">Entrez une lettre :</label>
+        <input type="text" name="lettre" id="lettre">
+        <br>
+        <br>
+        <input type="submit" value="Envoyer">
+
+    </form>
+    <br>
+    <br>
+    <?php
+
+    function occurence($mot, $lettre) {
+        $occurence = 0;
+        for ($i=0; $i < strlen($mot); $i++) {
+            if ($mot[$i] == $lettre) {
+                $occurence++;
+            }
+        }
+        return $occurence;
+    }
+
+    if (isset($_GET["mot"]) && isset($_GET["lettre"])) {
+        $mot = $_GET["mot"];
+        $lettre = $_GET["lettre"];
+        $occurence = occurence($mot, $lettre);
+        echo "La lettre $lettre apparait $occurence fois dans le mot $mot";
+    }
+
+    ?>
+
+</body>
+</html>
