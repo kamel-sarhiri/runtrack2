2d1cd9e10360ae17e4dfdc8d8be85284f7b2ce37
kamel-sarhiri <kamel.sarhiri@laplateforme.io>
2022-11-24 14:33:00 +0100

    réalisation requête job12,13 et 14

---
+++ jour09/job14/job14.sql
@@ -0,0 +1,3 @@
+-- requête permettant de sélectionner le prenom, le nom et la date de naissance des étudiants qui sont nés entre 1998 et 2018.
+--
+SELECT prenom, nom, naissance FROM etudiants WHERE naissance BETWEEN '1998-01-01' AND '2018-12-31';
